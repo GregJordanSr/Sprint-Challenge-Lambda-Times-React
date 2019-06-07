888261cbd534af92747ed50e117475d49f2135c4
Greg Jordan <gregljordan@gmail.com>
2019-06-07 13:00:05 -0400

    Tabs component completed

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,6 +1,17 @@
 ## Self-Study/Essay Questions
 
 - [ ] What are PropTypes used for? Please describe why it's important to type check our data in JavaScript.
+
+Proptypes are used for type checking and making sure that a specific data type is being used where you want it. It aids in preventing future errors.
+
 - [ ] Describe a life-cycle event in React?
+  
+  Methods that you use run code at particular times of the component state. It allows the developer to interact with the app in a very detailed way.
+
 - [ ] Explain the details of a Higher Order Component?
+
+It's a component that takes a component in as an argument
+
 - [ ] What are three different ways to style components in React? Explain some of the benefits of each.
+
+CSS stylesheet, inline styling, and styled-components or other type of libraries.  Each gives the developer the ability to control styling in a specific way and reuse the styling.
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -6,11 +6,26 @@ const Tabs = props => {
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+            props.tabs.map((tab, index) => {
+              return (
+                <Tab 
+                  key={index} 
+                  tab={tab}
+                  selectedTab={props.selectedTab}
+                  selectTabHandler={props.selectTabHandler}
+                />
+              )
+            })}     
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string
+};
 export default Tabs;
